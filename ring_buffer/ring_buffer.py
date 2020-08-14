a42d48f4db2b3a7d427ae00200fbd0f648384764
Justin Russell <Justin@justins-macbook.lan>
2020-08-14 08:09:23 -0700

    Ring buffer almost there

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,24 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.storage = []
+        self.capacity = capacity
+        self.num_items = 0
+        self.oldest = None
 
     def append(self, item):
-        pass
+        if self.storage:
+            if self.num_items < self.capacity:
+                self.storage.append(item)
+                self.num_items += 1
+            else:
+                self.storage[self.oldest] = item
+                self.oldest += 1
+                if self.oldest >= self.capacity:
+                    self.oldest = 0
+        else:
+            self.storage.append(item)
+            self.oldest = 0
+            self.num_items = 1
 
     def get(self):
-        pass
+        return self.storage
